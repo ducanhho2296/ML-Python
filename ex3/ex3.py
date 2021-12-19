ac8550c2394035243df3538253ec708a83ae1a5e
Duc Anh Ho <92146886+ducanhho2296@users.noreply.github.com>
2021-12-19 19:52:16 +0100

    update

--- ex3/ex3.py
+++ ex3/ex3.py
@@ -1,8 +1,11 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 from scipy.io import loadmat
 # Use loadmat to load matlab files
 mat=loadmat("ex3data1.mat")
 X=mat["X"]
 y=mat["y"]
+
+fig, axis = plt.subplots(10,10,figsize=(8,8))
