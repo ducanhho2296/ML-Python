15e3274a75201df529108902dd9b789f96b02184
Duc Anh Ho <92146886+ducanhho2296@users.noreply.github.com>
2021-12-22 20:37:32 +0100

    update

--- ex3/function.py
+++ ex3/function.py
@@ -11,9 +11,12 @@ def lrCostFuntion(theta, X, y, Lambda):
     regCost = cost + Lambda/(2 * m) * sum(theta[1:] ** 2)
 
     #gradient
-    j_0 = (1 / m) * (X.transpose() @ (predictions - y))[0]          # @: matrices multiplication
+    j_0 = (1 / m) * (X.transpose() @ (predictions - y))[0]    # @: matrices multiplication
     j_1 = (1 / m) * (X.transpose() @ (predictions - y))[1:] + (Lambda/m) * theta[1:]
     grad = np.vstack((j_0[:,np.newaxis],j_1))
     return regCost[0], grad
 
-def gradientDecent
+def gradientDecent(X,y theta, alpha,num_iters, Lambda):
+    m = len(y)
+    J_history = []
+
